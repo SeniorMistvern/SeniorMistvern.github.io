6ce151b6df131df49633391ed80707ccea4967c3
SeniorMistvern <roy_santander@hotmail.com>
2024-12-17 04:52:32 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://192.168.129.160/data')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('data').textContent = data.value;
-        })
-        .catch(error => console.error('Error:', error));
-});
